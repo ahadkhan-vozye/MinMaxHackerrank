af89b2c6de6957f308a0195d03659ab60831bb48
ahad <khansahab.ahad@gmail.com>
2022-04-22 02:52:50 +0500

    a bit of better approach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,15 @@
-import java.util.*
-
-
 fun maxMin(k: Int, arr: Array<Int>): Int {
     val unfairnessList = ArrayList<Int>()
     if (arr.isEmpty())
         return 0
 
-    val sortedList = arr.toList().sortedDescending()
+    val sortedList = arr.toList().sorted()
 
-    for (i in 0 until arr.size - k) {
+    for (i in 0 until arr.size - k + 1) {
         println(i)
         println(i + k)
-        val tempList = sortedList.subList(i, i + k)
-        val max = tempList.maxOrNull()!!
-        val min = tempList.minOrNull()!!
+        val max = sortedList[i + k - 1]
+        val min = sortedList[i]
         unfairnessList.add(max - min)
     }
 
@@ -24,31 +20,18 @@ fun maxMin(k: Int, arr: Array<Int>): Int {
 }
 
 fun main() {
-    val n = 20
+    val n = 7
 
-    val k = 5
+    val k = 3
 
     val arr = Array<Int>(n, { 0 })
-    arr[0] = 4504
-    arr[1] = 1520
-    arr[2] = 5857
-    arr[3] = 4094
-    arr[4] = 4157
-    arr[5] = 3902
-    arr[6] = 822
-    arr[7] = 6643
-    arr[8] = 2422
-    arr[9] = 7288
-    arr[10] = 8245
-    arr[11] = 9948
-    arr[12] = 2822
-    arr[13] = 1784
-    arr[14] = 7802
-    arr[15] = 3142
-    arr[16] = 9739
-    arr[17] = 5629
-    arr[18] = 5413
-    arr[19] = 7232
+    arr[0] = 100
+    arr[1] = 200
+    arr[2] = 300
+    arr[3] = 350
+    arr[4] = 400
+    arr[5] = 401
+    arr[6] = 402
 
     val result = maxMin(k, arr)
 
